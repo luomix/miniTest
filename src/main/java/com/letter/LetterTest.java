23f5acd64e40f5bf373c961cacd0cbbe54b3b594
luomix <948552118@qq.com>
2020-11-07 21:32:55 +0800

    功能实现

---
+++ src/main/java/com/letter/LetterTest.java
@@ -0,0 +1,11 @@
+package com.letter;
+
+import static org.junit.Assert.*;
+
+public class LetterTest {
+
+    @org.junit.Test
+    public void letterCombinations() {
+        System.out.println(Letter.letterCombinations("11112345"));;
+    }
+}
